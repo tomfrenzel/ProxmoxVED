1521741741cdd51e0ed9d69dd9d7141b54fc40a5
Tom Frenzel <admin@tom-frenzel.de>
2026-05-07 17:00:35 +0200

    add matterjs-server

---
+++ ct/matterjs-server.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVED/main/misc/build.func)
+# Copyright (c) 2021-2026 community-scripts ORG
+# Author: GitHub Copilot
+# License: MIT | https://github.com/community-scripts/ProxmoxVED/raw/main/LICENSE
+# Source: https://github.com/matter-js/matterjs-server
+
+APP="Matter.js Server"
+var_tags="${var_tags:-matter;iot;homeautomation}"
+var_cpu="${var_cpu:-4}"
+var_ram="${var_ram:-4096}"
+var_disk="${var_disk:-10}"
+var_os="${var_os:-debian}"
+var_version="${var_version:-13}"
+var_unprivileged="${var_unprivileged:-1}"
+
+header_info "$APP"
+variables
+color
+catch_errors
+
+function update_script() {
+  header_info
+  check_container_storage
+  check_container_resources
+
+  if [[ ! -d /opt/matterjs-server ]]; then
+    msg_error "No ${APP} Installation Found!"
+    exit
+  fi
+
+  if check_for_gh_release "matter-server" "matter-js/matterjs-server"; then
+    msg_info "Stopping Service"
+    systemctl stop matterjs-server
+    msg_ok "Stopped Service"
+
+    msg_info "Backing up Data"
+    cp -r /opt/matterjs-server/data /opt/matterjs-server_data_backup
+    msg_ok "Backed up Data"
+
+    CLEAN_INSTALL=1 fetch_and_deploy_gh_release "matter-server" "matter-js/matterjs-server" "tarball"
+
+    msg_info "Building Application"
+    cd /opt/matterjs-server
+    $STD npm install
+    $STD npm run build
+    msg_ok "Built Application"
+
+    msg_info "Restoring Data"
+    cp -r /opt/matterjs-server_data_backup/. /opt/matterjs-server/data
+    rm -rf /opt/matterjs-server_data_backup
+    msg_ok "Restored Data"
+
+    msg_info "Starting Service"
+    systemctl start matterjs-server
+    msg_ok "Started Service"
+    msg_ok "Updated successfully!"
+  fi
+  exit
+}
+
+start
+build_container
+description
+
+msg_ok "Completed Successfully!\n"
+echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
+echo -e "${INFO}${YW} Access it using the following URL:${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:5580${CL}"
---
+++ install/matterjs-server-install.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2026 community-scripts ORG
+# Author: GitHub Copilot
+# License: MIT | https://github.com/community-scripts/ProxmoxVED/raw/main/LICENSE
+# Source: https://github.com/matter-js/matterjs-server
+
+source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Installing Dependencies"
+$STD apt install -y \
+  build-essential
+msg_ok "Installed Dependencies"
+
+NODE_VERSION="22" setup_nodejs
+
+fetch_and_deploy_gh_release "matter-server" "matter-js/matterjs-server" "tarball"
+
+msg_info "Building Application"
+cd /opt/matterjs-server
+$STD npm install
+$STD npm run build
+msg_ok "Built Application"
+
+msg_info "Creating Data Directory"
+mkdir -p /opt/matterjs-server/data
+msg_ok "Created Data Directory"
+
+msg_info "Creating Service"
+cat <<EOF >/etc/systemd/system/matterjs-server.service
+[Unit]
+Description=Matter.js Server
+After=network.target
+
+[Service]
+Type=simple
+User=root
+WorkingDirectory=/opt/matterjs-server
+ExecStart=/usr/bin/node /opt/matterjs-server/packages/matter-server/dist/esm/MatterServer.js --storage-path /opt/matterjs-server/data --port 5580
+Restart=on-failure
+RestartSec=5
+
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl enable -q --now matterjs-server
+msg_ok "Created Service"
+
+motd_ssh
+customize
+cleanup_lxc
