e91e868a0880f6b27d4454a195d416541ac6338c
Tom Frenzel <40773830+tomfrenzel@users.noreply.github.com>
2026-09-17 21:04:58 +0200

    Update source loading mechanism in dmarcguard.sh

--- ct/dmarcguard.sh
+++ ct/dmarcguard.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-source <(curl -fsSL https://raw.githubusercontent.com/tomfrenzel/ProxmoxVED/dmarc-guard/misc/build.func)
+_cs_boot="${COMMUNITY_SCRIPTS_CORE_DIR:-$(dirname "${BASH_SOURCE[0]}")/../../core}/core/build.func"
+source "$_cs_boot" 2>/dev/null || source <(curl -fsSL "${COMMUNITY_SCRIPTS_CORE_URL:-https://raw.githubusercontent.com/community-scripts/core/main}/core/build.func")
 # Copyright (c) 2021-2026 community-scripts ORG
 # Author: Tom Frenzel (tomfrenzel)
 # License: MIT | https://github.com/community-scripts/ProxmoxVED/raw/main/LICENSE
